4d1189342fa50ab3786041774d633d3841933686
MatthewSDick <matthew.dick@outlook.com>
2020-02-11 15:44:43 -0500

    Adventure

--- Program.cs
+++ Program.cs
@@ -18,92 +18,183 @@ namespace RPSSpock
 
       //Code welcome message
       Console.WriteLine("Welcome to rock, paper scissors.");
-      Console.WriteLine("Please choose one of the following: rock, paper or scissor");
+      var computerChoice = "nothing";
+      var winnerMessage = "nothing";
+      Boolean playing = true;
 
-      var userChoice = Console.ReadLine().ToLower();
-
-      Boolean good = false;
-
-
-      while (good == false)
+      while (playing == true)
       {
+
+        // Choose difficulty level and validation logic
+        Console.WriteLine("Please choose a difficulty level:one of the following: normal, impossible and easy");
+        var userDifficulty = Console.ReadLine().ToLower();
+
+        Boolean difficultySelected = false;
+
+        while (difficultySelected == false)
+        {
+          if (userDifficulty == "normal" || userDifficulty == "impossible" || userDifficulty == "easy")
+          {
+            difficultySelected = true;
+          }
+          else
+          {
+            Console.WriteLine("Again moron...Please choose a difficulty level:one of the following: normal, impossible and easy");
+            userDifficulty = Console.ReadLine().ToLower();
+          }
+
+        }
+
+        Console.WriteLine("Please choose one of the following: rock, paper or scissor");
+
+        var userChoice = Console.ReadLine().ToLower();
+
+        Boolean good = false;
+
+        while (good == false)
+        {
+          if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissor")
+          {
+            good = true;
+          }
+          else
+          {
+            Console.WriteLine("Again...Please choose one of the following: rock, paper or scissor");
+            userChoice = Console.ReadLine().ToLower();
+          }
+        }
+
+        Console.WriteLine("Thank you.  You have selected " + userChoice);
+
+        switch (userDifficulty)
+        {
+
+          case "impossible":
+            Console.WriteLine("You chose Impossible");
+
+            switch (userChoice)
+            {
+              case "rock":
+                computerChoice = "paper";
+                break;
+
+              case "paper":
+                computerChoice = "scissor";
+                break;
+
+              default:
+                computerChoice = "rock";
+                break;
+            }
+            break;
+
+          case "easy":
+            Console.WriteLine("Easy");
+
+            switch (userChoice)
+            {
+              case "rock":
+                computerChoice = "scissor";
+                break;
+
+              case "paper":
+                computerChoice = "rock";
+                break;
+
+              default:
+                computerChoice = "paper";
+                break;
+            }
+            break;
+
+          default:
+            Console.WriteLine("Normal");
+
+            // Testing
+            //userChoice = "rock";
+
+            // This is where we use the level
+
+
+
+            //var computerChoice = new Random();
+
+            Random rnd = new Random();
+            string[] gameChoice = { "rock", "paper", "scissor" };
+            int mIndex = rnd.Next(gameChoice.Length);
+            Console.WriteLine("I selected {0}", gameChoice[mIndex]);
+            computerChoice = gameChoice[mIndex];
+            break;
+
+
+
+
+
+
+
+
+
+
+            // end pick level
+
+        }
+        if (userChoice == computerChoice)
+        {
+          winnerMessage = "we tied.";
+        }
+
         if (userChoice == "rock")
         {
-          good = true;
+          if (computerChoice == "scissor")
+          {
+            winnerMessage = "you win.";
+          }
+          else if (computerChoice == "paper")
+          {
+            winnerMessage = "I win.";
+          }
         }
-        else if (userChoice == "paper")
-        {
-          good = true;
-        }
-        else if (userChoice == "scissor")
-        {
-          good = true;
-        }
-        else
-        {
-          Console.WriteLine("Again...Please choose one of the following: rock, paper or scissor");
-          userChoice = Console.ReadLine().ToLower();
 
+        if (userChoice == "paper")
+        {
+          if (computerChoice == "scissor")
+          {
+            winnerMessage = "I win.";
+          }
+          else if (computerChoice == "rock")
+          {
+            winnerMessage = "you win.";
+          }
         }
+
+        if (userChoice == "scissor")
+        {
+          if (computerChoice == "rock")
+          {
+            winnerMessage = "I win.";
+          }
+          else if (computerChoice == "paper")
+          {
+            winnerMessage = "you win.";
+          }
+        }
+
+        Console.WriteLine("Based on your choice of " + userChoice + " and my choice of " + computerChoice + ", " + winnerMessage);
+
+        Console.WriteLine("Dow you want to play again? Yes or No");
+        var playAgain = Console.ReadLine().ToLower();
+
+        if (playAgain == "yes")
+        {
+          playing = true;
+        }
+        else if (playAgain == "no")
+        {
+          playing = false;
+          Console.Write("Thank you for playing");
+        }
+
       }
-
-      // Testing
-      //userChoice = "rock";
-
-      Console.WriteLine("Thank you.  You have selected " + userChoice);
-
-      //var computerChoice = new Random();
-
-      Random rnd = new Random();
-      string[] gameChoice = { "rock", "paper", "scissor" };
-      int mIndex = rnd.Next(gameChoice.Length);
-      Console.WriteLine("I selected {0}", gameChoice[mIndex]);
-
-      var winnerMessage = "nothing";
-
-      if (userChoice == gameChoice[mIndex])
-      {
-        winnerMessage = "we tied.";
-      }
-
-      if (userChoice == "rock")
-      {
-        if (gameChoice[mIndex] == "scissor")
-        {
-          winnerMessage = "you win.";
-        }
-        else if (gameChoice[mIndex] == "paper")
-        {
-          winnerMessage = "I win.";
-        }
-      }
-
-      if (userChoice == "paper")
-      {
-        if (gameChoice[mIndex] == "scissor")
-        {
-          winnerMessage = "I win.";
-        }
-        else if (gameChoice[mIndex] == "rock")
-        {
-          winnerMessage = "you win.";
-        }
-      }
-
-      if (userChoice == "scissor")
-      {
-        if (gameChoice[mIndex] == "rock")
-        {
-          winnerMessage = "I win.";
-        }
-        else if (gameChoice[mIndex] == "paper")
-        {
-          winnerMessage = "you win.";
-        }
-      }
-
-      Console.WriteLine("Based on your choice of " + userChoice + " and my choice of " + gameChoice[mIndex] + ", " + winnerMessage);
-
     }
   }
 }
